4f4abe338f8eda5f90359f5cff5136e1b50b0442
AaronGoddard <aaronbear@gmail.com>
2018-08-31 18:01:30 -0700

    Begin lru implementation

--- lib/p06_lru_cache.rb
+++ lib/p06_lru_cache.rb
@@ -14,7 +14,15 @@ class LRUCache
     @map.count
   end
 
-  def get(key)
+  def get(key) #Of Fenris!
+    unless @map.include?(key)
+      val = @prc.call(key)
+      @store.append(key, val)
+      @map.set(key, val)
+    end
+    # node = @map.get(key)
+    eject! if @map.count > @max
+     val
   end
 
   def to_s
@@ -32,5 +40,7 @@ class LRUCache
   end
 
   def eject!
+    #@map[0].delete
+    @store.remove(@store.head.next.key)
   end
 end
